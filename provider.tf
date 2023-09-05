9cd947653b519e39c891e013602f35ad46adfcfa
Gaurav Rai <Gaurav.Rai@ve3.global>
2023-09-05 11:13:19 +0000

    Updated provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,9 @@
 terraform {
   backend "azurerm" {
+    resource_group_name  = "test-pipeline"
+    storage_account_name = "terraformsg1"
+    container_name       = "terraform-state"
+    key                  = "terraform.tfstate"
   }
 }
 provider "azurerm" {
